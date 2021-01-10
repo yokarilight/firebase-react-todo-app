ec1a6bb8f889a3f4ab5005cc1f0f9bd8dda2123c
yokarilight <yokarilight@gmail.com>
2021-01-11 00:50:54 +0800

    replace info in config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,6 @@
 export const config = {
-  apiKey: "AIzaSyDlYg24-dklqq9NyBbQRxED9mB0H2gN50w",
-  authDomain: "fir-todo-app-74716.firebaseapp.com",
-  databaseURL: "https://fir-todo-app-74716-default-rtdb.firebaseio.com/",
-  projectId: "fir-todo-app-74716",
+  apiKey: "<API_KEY>",
+  authDomain: "<PROJECT_ID>.firebaseapp.com",
+  databaseURL: "https://<DATABASE_NAME>.firebaseio.com/",
+  projectId: "<PROJECT_ID>",
 };
